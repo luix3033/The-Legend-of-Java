f558c4c2be61e088f94866c340787cb8f18e0491
Usuario <Usuario@192.168.1.233>
2026-06-04 09:55:06 -0300

    implementado sistema de savegame

--- game_01/src/com/treszerotresstudios/main/Menu.java
+++ game_01/src/com/treszerotresstudios/main/Menu.java
@@ -5,9 +5,17 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+
+import com.treszerotresstudios.world.World;
 
 public class Menu {
 	
@@ -16,7 +24,7 @@ public class Menu {
 	public int currentOption = 0;
 	public int maxOption = options.length -1;
 	public boolean up,down,enter;
-	public boolean pause = false;
+	public static boolean pause = false, saveExists = false, saveGame=false;
 	
 	
 	private BufferedImage menuBackground;
@@ -31,6 +39,13 @@ public class Menu {
 	
 	
 	public void tick() {
+		File file = new File("save.txt");
+		if(file.exists()) {
+			saveExists = true;
+			
+		}else {
+			saveExists = false;
+		}
 		if(up) {
 			up = false;
 			currentOption--;
@@ -53,11 +68,100 @@ public class Menu {
 			if(options[currentOption] == "Novo Jogo" || options[currentOption] == "Continuar") { 			
 				Game.gameState = "NORMAL";
 				pause = false;
+				 file = new File("save.txt");
+				file.delete();
 				
-			} else if(options[currentOption]== "Sair") {
+			}  else if(options[currentOption] == "Carregar Jogo"){
+				file = new File("save.txt");
+				if(file.exists()) {
+					String saver = loadGame(10);
+					applySave(saver);
+				}
+			}
+			
+			else if(options[currentOption]== "Sair") {
 				System.exit(1);
 			}
 		}
+	}
+	
+	
+	public static void applySave(String str) {
+		String[] spl = str.split("/");
+		for(int i = 0; i < spl.length; i++) {
+			String[] spl2 = spl[i].split(":");
+			switch(spl2[0]) {
+			case "level":
+				World.restartGame("level"+spl2[1]+".png");
+				Game.gameState= "NORMAL";
+				pause = false;
+				break;
+			case "vida":
+				Game.player.life = Integer.parseInt(spl2[1]);
+				break;
+			}
+			
+		}
+	}
+	
+	public static String loadGame(int encode) {
+		String line = "";
+		File file = new File("save.txt");
+		if(file.exists()) {
+			try {
+				String singleLine = null;
+				BufferedReader reader =new BufferedReader(new FileReader("save.txt"));
+				try {
+					while((singleLine = reader.readLine()) != null) {
+						String[] trans = singleLine.split(":");
+						char[] val = trans[1].toCharArray();
+						trans[1] = "";
+						for(int i = 0; i < val.length; i++) {
+							val[i]-=encode;
+							trans[1]+=val[i];
+						}
+						line+=trans[0];
+						line+=":";
+						line+=trans[1];
+						line+="/";
+					}
+				}catch(IOException e) {}
+			} catch(FileNotFoundException e) {}
+		}
+		
+		return line;
+	}
+	
+	
+	public static void saveGame(String[] val1,int[] val2, int encode) {
+		BufferedWriter write = null;
+		try {
+			write = new BufferedWriter(new FileWriter("save.txt"));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(int i = 0; i < val1.length; i++) {
+			String current = val1[i];
+			current+=":";
+			char[] value = Integer.toString(val2[i]).toCharArray();
+			for(int n = 0; n< value.length;n++) {
+				value[n]+=encode;
+				current+=value[n];				
+			}
+			try {
+				write.write(current);
+				if(i < val1.length - 1)
+					write.newLine();
+			}catch(IOException e) {}
+		}
+		try {
+			write.flush();
+			write.close();
+		} catch(IOException e) {
+			
+		}
+		
 	}
 	
 	public void render(Graphics g) {
